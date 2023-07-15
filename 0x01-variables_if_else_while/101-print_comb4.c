eb3b92271e0f210870a517d577ad178c7181d24b
samxr7 <samxr289@gmail.com>
2023-07-14 18:21:29 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,7 +15,7 @@ digit2 = 0;
 while (digit2 <= 9)
 {
 digit3 = 0;
-while(digit3 <= 9)
+while (digit3 <= 9)
 {
 if (digit1 != digit2 &&
 digit1 < digit2 &&
@@ -28,7 +28,7 @@ putchar(digit3 + 48);
 if (digit1 + digit2 + digit3 != 24)
 {
 putchar(',');
-putchar(',');
+putchar(' ');
 }
 }
 digit3++;
@@ -40,4 +40,3 @@ digit1++;
 putchar('\n');
 return (0);
 }
-
